1f1e0769f81c9c3bb670cd0a62cbf99c8e1a2324
JP Kabler <jp.kabler@orchestro.com>
2015-04-27 19:59:47 -0500

    moving through chapter 1

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -10,3 +10,27 @@
   []
   (println "Choo choo!"))
 
+(if true
+  "abra cadabra"
+  "hocus pocus")
+
+(if true 
+  (do (println "Success!")
+      "abra cadabra")
+  (do (println "Failure :(")
+      "hocus pocus"))
+
+(when true
+  (println "Success!")
+  "abra cadabra")
+
+(def failed-protagonist-names
+  ["Larry Potter"
+   "Doreen the Explorer"
+   "The incredible bulk"])
+
+(def severity :mild)
+(def error-message "Oh god.  it's a disaster we're ")
+(if (= severity :mild)
+  (def error-message (str error-message "MILDLY INCONVENIENCED"))
+  (def error-message (str error-message "doomed")))
